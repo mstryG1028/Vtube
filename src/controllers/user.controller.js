cdc86fc12811eaa239c4f93081c26a823d18b384
deepak mistry <mistrydeepak1028@gmail.com>
2026-06-08 22:25:03 +0530

    learn errClass

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,42 @@
+import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OK",
-  });
-});
+  // steps to create registerUser
+  //1. get user details from frontend
+  const { fullName, username, email, password } = req.body; // req.body form ya phit json data ko acces karta hai
+  console.log(fullName, email);
 
+  //2.validation
+
+  // ye beginners ke liye hai aise hi password email sabke liye check kar lena hai if lga ke
+  // if(fullName===""){
+  //   throw new ApiError(400,"fullName is required")
+  // }
+
+  //but one shortcut is
+
+  if([fullName,email,username,password].some((field)={field?.trim()===""}))
+
+
+  //3. check if user exist or not
+  //4 check for images, check for avatar
+  //5 upload them to cloudinary
+  //6 create user object - create entry in db
+  //7 remove password and refreshToken fields from response
+  //8 check for user creation
+  //9 return response
+
+  // let newUser = new User({
+  //   username,
+  //   email,
+  //   password,
+  // });
+  // await newUser.save();
+});
 
 // yahan pe control app.js--> user.routes.js--> user.controller.js se aa rha hai
 // app.js me prefix path define karte hai like "/api/v1/users"
-// routes me suffix path like regiter, login 
+// routes me suffix path like regiter, login
 // /users/login pe kya karna hai wo controller me
 export { registerUser };
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,9 +1,15 @@
 // Assignment complete file
+// This class is used to create custom API errors with a consistent structure across your backend.
+// this is used to maintain consistency throughout the application
+// how any error will look like which fields it should contains
+// it increases the code reusability
+
+//ex:
 
 class ApiError extends Error {
   constructor(
     statusCode,
-    message = "Something went wrong",
+    message = "Something went wrong",// if no message is passed by user the by default this will sended
     errors = [],
     stack = ""
   ) {
@@ -23,3 +29,113 @@ class ApiError extends Error {
 }
 
 export {ApiError}
+
+
+/*
+
+Middleware defines HOW the response is sent.
+
+ApiError defines WHAT data the error object contains.
+
+
+--------------------------------------------------
+diff way to throw err
+--------------------------------------------------
+1. using built in class of err
+
+Controller:
+
+if (existedUser) {
+    throw new Error("User already exists");
+}
+
+response:
+{
+    message: "User already exists"
+}
+
+2. Middleware:
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        message: err.message
+    });
+});
+
+Controller:
+
+if (existedUser) {
+    const err = new Error("User already exists");
+    err.statusCode = 409;
+    throw err;
+}
+
+Problems:
+- every time hame  itna code likhna padega jab bhi hame err throw karna hai
+- isliye jab hame ek-do baar hi use karna ho to thi hai ye wala
+if (existedUser) {
+    const err = new Error("User already exists");
+    err.statusCode = 409;
+    throw err;
+}
+
+--------------------------------------------------
+CASE 3: ApiError
+--------------------------------------------------
+
+Create once:
+
+class ApiError extends Error {
+    constructor(statusCode, message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+Controller:
+
+throw new ApiError(409, "User already exists");
+
+isme hame less code likhna padta hai bas ek line like hai 
+iska response same as middleware hoga
+
+--------------------------------------------------
+THE REAL BENEFIT APPEARS LATER
+--------------------------------------------------
+Suppose you want every error to have:
+
+{
+    success: false,
+    message: "...",
+    errors: [],
+    statusCode: ...
+}
+
+Without ApiError:
+
+const err = new Error("Validation failed");
+err.statusCode = 400;
+err.success = false;
+err.errors = ["Email required"];
+throw err;
+
+You'll repeat this everywhere.
+
+With ApiError:
+
+throw new ApiError(
+    400,
+    "Validation failed",
+    ["Email required"]
+);
+
+The class automatically creates:
+
+{
+    statusCode: 400,
+    success: false,
+    message: "Validation failed",
+    errors: ["Email required"]
+}
+
+*/
